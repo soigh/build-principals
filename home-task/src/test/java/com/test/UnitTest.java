2a2beab17c4f1365a5ca176b1173c4a54ec99ef7
Aleksandr Mazurenko <neo-katso@yandex.ru>
2017-06-25 23:32:03 +0300

    Update UnitTest.java
    extending tests

--- home-task/src/test/java/com/test/UnitTest.java
+++ home-task/src/test/java/com/test/UnitTest.java
@@ -1,9 +1,15 @@
 package com.test;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class UnitTest {
-   
+    @Test public void testAppHasAGreeting() {
+        Project classUnderTest = new Project();
+        assertNotNull("app should have a greeting",
+                       classUnderTest.getGreeting());
+    }
+
    @Test
    public void test(){
      Project.test();
