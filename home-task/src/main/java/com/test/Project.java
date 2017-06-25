376b63888c9c34ad967d734c53f8378852703ae6
Aleksandr Mazurenko <neo-katso@yandex.ru>
2017-06-25 23:30:46 +0300

    Update Project.java
    refactoring

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -1,16 +1,18 @@
 package com.test;
 
-//dependency on Apache commons-collections
 import org.apache.commons.collections.Predicate;
 
 class Project {
 
+ public String getGreeting() {
+        return "Hello, MNT Lab!";
+    }
+
   public static void test() {
     System.out.println("test");
   }
-  
-  public static void main(String[] args) {
-    System.out.println("Hello MNT Lab");
-  }
 
+  public static void main(String[] args) {
+    System.out.println(new Project().getGreeting());
+  }
 }
