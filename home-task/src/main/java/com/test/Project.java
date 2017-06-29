8f14078d388057118057e0b3a743d5e89c35cbbb
untiro <untiro@bk.ru>
2017-06-29 14:15:01 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -12,8 +12,12 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
+ 
+   public static void test2() {
+    System.out.println"test";
+  }
 
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
-}
+
