a1cbbb579cda6154ad0ac40619daf8ef3f93f2f4
artem-aksenkin <artemaksenkin@gmail.com>
2017-06-29 13:20:10 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -12,7 +12,17 @@ class Project {
   public static void test() {
     System.out.println("test");
   }
-
+  public static void test() {
+    System.out.println("test");
+}  public static void test() {
+    System.out.println("test");
+}  public static void test() {
+    System.out.println("test");
+}  public static void test() {
+    System.out.println("test");
+}  public static void test() {
+    System.out.println("test");
+}
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
