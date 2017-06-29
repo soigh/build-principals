887757a87383b8f2c95ed3e249a589628281e6f5
Alexey Semirski <al.semirski@gmail.com>
2017-06-29 14:16:59 +0300

    bug code added

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -16,4 +16,19 @@ class Project {
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
+ public static void switchCasePrimer() {
+    	int caseIndex = 0;
+    	switch (caseIndex) {
+        	case 0:
+            	System.out.println("Zero");
+        	case 1:
+            	System.out.println("One");
+            	break;
+        	case 2:
+            	System.out.println("Two");
+            	break;
+        	default:
+            	System.out.println("Default");
+    	}
+ }
 }
