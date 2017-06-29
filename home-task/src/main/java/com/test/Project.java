97761e376f8d3f2a12935d9f2129776c0b5f79bb
untiro <untiro@bk.ru>
2017-06-29 14:24:32 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -13,11 +13,16 @@ class Project {
     System.out.println("test");
   }
  
-   public static void test2() {
-    System.out.println"test";
-  }
+Date d = new Date();
+d.setDate(25);
+d.setYear(2014);
+d.setMonth(12);  // Noncompliant; rolls d into the next year
 
+Calendar c = new GregorianCalendar(2014, 12, 25);  // Noncompliant
+if (c.get(Calendar.MONTH) == 12) {  // Noncompliant; invalid comparison
+  // ...
+}
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
-
+}
