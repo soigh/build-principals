8f021a000bee10ffadf82553b1b3056db2dcf514
Andrew Doropey <andry.doropey@gmail.com>
2017-06-29 18:28:44 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -3,10 +3,28 @@ package com.test;
 import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
+/**
+ * @deprecated  As of release 1.3, replaced by {@link #Fee}
+ */@Deprecated
+class Fum { ... }
+
+public class Bar extends Fum {  // Noncompliant; Fum is deprecated
+
+  public void myMethod() {
+    Foo foo = new Foo();  // okay; the class isn't deprecated
+    foo.doTheThing();  // Noncompliant
+  }
+}
+
 class Project {
 
  public String getGreeting() {
         return "Hello, MNT Lab!";
+        int a = 1;
+        while(true)
+        {
+          a++;
+        }
     }
 
   public static void test() {
