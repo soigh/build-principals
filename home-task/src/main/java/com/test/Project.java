961ee67ec664039e067df8c924e9de86f743f4a6
untiro <untiro@bk.ru>
2017-06-30 14:43:17 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -3,10 +3,8 @@ package com.test;
 import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
-
 class Project {
 
-  
  public String getGreeting() {
         return "Hello, MNT Lab!";
     }
@@ -15,15 +13,8 @@ class Project {
     System.out.println("test");
   }
 
- public static void main(String[] args) {
+  public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
-   if (param == 1)
-  openWindow();
-else if (param == 2)
-  closeWindow();
-else if (param == 1)  // Noncompliant
-  moveWindowToTheBackground();
-}
   }
 }
 
