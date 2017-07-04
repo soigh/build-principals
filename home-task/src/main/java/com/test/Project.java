529ca64f336d139dd1e247986c474db0ce1870c2
artem-aksenkin <artem_askenkin@gmail.co>
2017-07-04 08:59:30 +0100

    Project .java changes

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -3,13 +3,17 @@ package com.test;
 import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
-class Project{
+class Project {
 
- public String getasdGreeting() {
+ public String getGreeting() {
         return "Hello, MNT Lab!";
     }
-}
+
+  public static void test() {
+    System.out.println("test");
+  }
+
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
-  }
+}
 }
