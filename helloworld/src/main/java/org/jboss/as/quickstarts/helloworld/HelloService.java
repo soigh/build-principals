ef8688bfb2929fe63b2f42ab6e3bc1a0ada956cc
PaulYurchuk <paulyurchuk@gmail.com>
2017-07-11 15:59:24 +0300

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello " + name + "! System shutting down";
+        return "Hello " + name + "! System shutting down..";
     }
 
 }
