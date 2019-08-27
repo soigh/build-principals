39845b602993e697b10be11cc2cc255667a24094
Oksana <53932708+soigh@users.noreply.github.com>
2019-08-27 15:44:58 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -16,4 +16,7 @@ class Project {
   public static void main(String[] args) {
     System.out.println(new Project().getGreeting());
   }
+ public String getGreeting() {
+        return "Hello, MNT Lab!";
+    }
 }
