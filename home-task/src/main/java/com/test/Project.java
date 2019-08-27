3bba13324b123f37698a771a34ba0f08e408ccbe
Oksana <53932708+soigh@users.noreply.github.com>
2019-08-27 16:10:40 +0300

    Update Project.java

--- home-task/src/main/java/com/test/Project.java
+++ home-task/src/main/java/com/test/Project.java
@@ -2,6 +2,8 @@ package com.test;
 
 import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
+import org.apache.commons.collections.Predicate;
+import org.apache.log4j.Logger;
 
 class Project {
 
